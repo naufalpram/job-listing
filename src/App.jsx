05af10427b5e95ed584f260e3e2d9e04fd8a02b0
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-02-26 17:05:36 +0700

    refactor: migrate to react router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import JobListing from './pages/JobListing';
-import {Route,  Routes} from 'react-router-dom';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import JobDetail from './pages/JobDetail';
 import Header from './components/Header';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />
+  },
+  {
+    path: '/jobs',
+    element: <Header />,
+    children: [
+      {
+        index: true,
+        element: <JobListing />
+      },
+      {
+        path: ':id',
+        element: <JobDetail />
+      }
+    ]
+  }
+]);
+
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/jobs' element={<Header />}>
-        <Route index element={<JobListing />} />
-        <Route path=':id' element={<JobDetail />}/>
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
     <header className="flex justify-center items-center bg-no-repeat bg-cover max-h-[20vh]">
-        <img
-          className="max-h-[20vh]"
-          src="https://assets-global.website-files.com/622b063e1c5d763e016af5ee/64806bb59d2c2cbcf0cf376c_Lister.png"
-          alt="logo"
-        />
+        <Link to='/'>
+            <img
+            className="max-h-[20vh]"
+            src="https://assets-global.website-files.com/622b063e1c5d763e016af5ee/64806bb59d2c2cbcf0cf376c_Lister.png"
+            alt="logo"
+            />
+        </Link>
     </header>
     <Outlet />
     </>
